367028792ce9beb33fe8fda5939dfaa56c429685
vahidtvj <vahid.tvj@gmail.com>
2024-07-26 22:25:58 +0330

    PWA + HTTPS

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from "vite"
+import fs from "node:fs"
+import path from "node:path"
 import react from "@vitejs/plugin-react-swc"
-
+import { defineConfig, loadEnv } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "")
+	const useHttps =
+		env.USE_HTTPS && env.USE_HTTPS === "true" && env.CERT_FILE && env.KEY_FILE
+	return {
+		plugins: [
+			react(),
+			VitePWA({
+				registerType: "autoUpdate",
+			}),
+		],
+		preview: {
+			https: useHttps
+				? {
+						key: fs.readFileSync(path.resolve(__dirname, env.KEY_FILE)),
+						cert: fs.readFileSync(path.resolve(__dirname, env.CERT_FILE)),
+					}
+				: undefined,
+		},
+	}
 })
